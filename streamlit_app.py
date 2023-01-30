8f39179cf0c0bbf513a379fbc47ec641547e3a5e
Frédérik Dufour <frederk.dufour25@gmail.com>
2023-01-30 14:36:12 -0500

    Update streamlit_app.py
    changed the index for my_fruit_list

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 ## fuit list
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
+my_fruit_list.set_index("Fruit")
 
 ## app components
 streamlit.title("My parents new healty diner")
